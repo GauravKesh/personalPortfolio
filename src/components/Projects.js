7faf5a3af7444821628451b24e3e6ded7cb5d04f
GauravKesh <gkrcoder@gmail.com>
2024-03-09 00:40:08 +0530

    added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import project1 from "../assets/projectImage/project1.png";
 import project2 from "../assets/projectImage/project2.png";
@@ -224,7 +225,10 @@ export default function Projects() {
                     </h1>
                     <div class="flex items-center flex-wrap space-x-2 py-5">
                       {/* github link */}
-                      <a class="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900">
+                      <a
+                        class="flex gap-3 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-800 to-black px-7 py-3 rounded-full border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900"
+                        href="https://github.com/GauravKesh/onyx"
+                      >
                         <svg
                           viewBox="0 0 24 24"
                           height="24"
