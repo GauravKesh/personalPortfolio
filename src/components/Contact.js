6850273c49fdca4432a8dec8a943380510a4a56d
GauravKesh <gkrcoder@gmail.com>
2024-03-23 14:57:19 +0530

    ..

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -226,7 +226,7 @@ export default function Contact() {
                         </label>
                         <input
                           type="text"
-                          placeholder="John Doe"
+                          placeholder="Gaurav Kumar"
                           name="name"
                           {...register("name")}
                           class="block w-full px-5 py-3 mt-2 text-gray-700  border border-gray-200 rounded-md bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
